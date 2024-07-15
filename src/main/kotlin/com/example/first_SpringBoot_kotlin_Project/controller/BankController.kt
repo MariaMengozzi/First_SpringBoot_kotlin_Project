60746215c24d50750cd680fb77445a90d25ac7cf
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-15 17:14:14 +0200

    feat-test: add get bank route and test that verify whether it exists or it does't

--- src/main/kotlin/com/example/first_SpringBoot_kotlin_Project/controller/BankController.kt
+++ src/main/kotlin/com/example/first_SpringBoot_kotlin_Project/controller/BankController.kt
@@ -2,13 +2,24 @@ package com.example.first_SpringBoot_kotlin_Project.controller
 
 import com.example.first_SpringBoot_kotlin_Project.model.Bank
 import com.example.first_SpringBoot_kotlin_Project.service.BankService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/banks")
 class BankController(private val service: BankService) {
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
+        ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
     @GetMapping
     fun getBanks() : Collection<Bank> = service.getBanks()
+
+    @GetMapping("/{accountNumber}") //must match the fun parameter name
+    fun getBank(@PathVariable accountNumber: String): Bank = service.getBank(accountNumber)
 }
--- src/main/kotlin/com/example/first_SpringBoot_kotlin_Project/datasource/BankDataSource.kt
+++ src/main/kotlin/com/example/first_SpringBoot_kotlin_Project/datasource/BankDataSource.kt
@@ -4,4 +4,5 @@ import com.example.first_SpringBoot_kotlin_Project.model.Bank
 
 interface BankDataSource {
     fun retrieveBanks(): Collection<Bank>
+    fun retrieveBank(accountNumber: String) : Bank
 }
