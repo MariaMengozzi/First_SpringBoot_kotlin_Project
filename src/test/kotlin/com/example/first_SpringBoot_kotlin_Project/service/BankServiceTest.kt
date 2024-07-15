d303753546485ebfa8ab5b3561783fec0fa6750a
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-15 16:18:20 +0200

    test: add service layer test

--- src/test/kotlin/com/example/first_SpringBoot_kotlin_Project/service/BankServiceTest.kt
+++ src/test/kotlin/com/example/first_SpringBoot_kotlin_Project/service/BankServiceTest.kt
@@ -1,5 +1,24 @@
 package com.example.first_SpringBoot_kotlin_Project.service
 
+import com.example.first_SpringBoot_kotlin_Project.datasource.BankDataSource
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class BankServiceTest
+internal class BankServiceTest{
+    /*A Mock Object is an object that substitutes for a real object. In object-oriented programming,
+    mock objects are simulated objects that mimic the behavior of real objects in controlled ways*/
+
+    private val dataSource: BankDataSource = mockk(relaxed = true) //return a default value - in this case an empty list
+    private val bankService = BankService(dataSource)
+    @Test
+    fun `should call its data source to retrieve banks`() {
+        // when
+        bankService.getBanks()
+        // then
+        verify(exactly = 1) {dataSource.retrieveBanks()}
+    }
+    
+}
