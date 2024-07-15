7e06f012d1464e4065ba7cf805e87bffae788526
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-15 16:17:49 +0200

    feat: add service layer

---
+++ src/main/kotlin/com/example/first_SpringBoot_kotlin_Project/service/BankService.kt
@@ -0,0 +1,12 @@
+package com.example.first_SpringBoot_kotlin_Project.service
+
+import com.example.first_SpringBoot_kotlin_Project.datasource.BankDataSource
+import com.example.first_SpringBoot_kotlin_Project.model.Bank
+import org.springframework.stereotype.Service
+
+@Service
+class BankService(private val dataSource: BankDataSource) {
+
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+
+}
