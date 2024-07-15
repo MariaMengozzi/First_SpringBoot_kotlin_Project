f73d9b188d7ad3d355c928e70cb8c59d171f7652
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-15 16:17:06 +0200

    feat: add test dependency - mock objects

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("io.mockk:mockk:1.10.4")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
---
+++ src/test/kotlin/com/example/first_SpringBoot_kotlin_Project/service/BankServiceTest.kt
@@ -0,0 +1,5 @@
+package com.example.first_SpringBoot_kotlin_Project.service
+
+import org.junit.jupiter.api.Assertions.*
+
+class BankServiceTest
