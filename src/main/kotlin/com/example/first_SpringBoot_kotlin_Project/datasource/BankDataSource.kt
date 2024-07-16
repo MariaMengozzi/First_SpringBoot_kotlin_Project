465849a33d1b91a4c6cd1aec1e6c037445dcce55
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-16 12:11:58 +0200

    feat: add post method for creating a new bank

--- src/main/kotlin/com/example/first_SpringBoot_kotlin_Project/datasource/BankDataSource.kt
+++ src/main/kotlin/com/example/first_SpringBoot_kotlin_Project/datasource/BankDataSource.kt
@@ -5,4 +5,5 @@ import com.example.first_SpringBoot_kotlin_Project.model.Bank
 interface BankDataSource {
     fun retrieveBanks(): Collection<Bank>
     fun retrieveBank(accountNumber: String) : Bank
+    fun createBank(bank: Bank): Bank
 }
--- src/test/kotlin/com/example/first_SpringBoot_kotlin_Project/controller/BankControllerTest.kt
+++ src/test/kotlin/com/example/first_SpringBoot_kotlin_Project/controller/BankControllerTest.kt
@@ -1,5 +1,8 @@
 package com.example.first_SpringBoot_kotlin_Project.controller
 
+import com.example.first_SpringBoot_kotlin_Project.model.Bank
+import com.fasterxml.jackson.databind.ObjectMapper
+import jdk.jfr.ContentType
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
@@ -11,13 +14,19 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 
 @SpringBootTest //it create the whole spring project here we are testing the REST API
 @AutoConfigureMockMvc
-internal class BankControllerTest {
+internal class BankControllerTest @Autowired constructor(
+     val mockMvc: MockMvc,
+     val objectMapper: ObjectMapper
+) {
 
-    @Autowired //with this spring boot does the dependency injection
-    lateinit var mockMvc: MockMvc //this should use only in tests
+    //@Autowired //with this spring boot does the dependency injection
+    //lateinit var mockMvc: MockMvc //this should use only in tests -> moved to constructor
+
+
 
     val baseUrl = "/api/banks"
 
@@ -71,6 +80,52 @@ internal class BankControllerTest {
                 .andDo{print()}
                 .andExpect{
                     status {isNotFound()}
+                }
+        }
+    }
+
+    @Nested
+    @DisplayName("POST /api/bank")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class PostNewBank {
+        @Test
+        fun `should add a new bank`() {
+            //given
+            val newBank = Bank("acc123", 31.415, 2)
+            //when
+            val performPost = mockMvc.post(baseUrl){
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(newBank)
+            }
+            //then
+            performPost.andDo { print() }
+            .andExpect {
+                status { isCreated() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$.accountNumber"){value("acc123")}
+                jsonPath("$.trust"){value("31.415")}
+                jsonPath("$.transactionFee"){value("2")}
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("POST /api/bank - BAD REQUEST")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class PostNewBankBadRequest {
+        @Test
+        fun `should return BAD REQUEST if bank with the given number already exist`() {
+            //given
+            val invalidBank = Bank("1234", 1.0, 1)
+            //when
+            val performPost = mockMvc.post(baseUrl){
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(invalidBank)
+            }
+            //then
+            performPost.andDo { print() }
+                .andExpect {
+                    status { status { isBadRequest() } }
                 }
         }
     }
