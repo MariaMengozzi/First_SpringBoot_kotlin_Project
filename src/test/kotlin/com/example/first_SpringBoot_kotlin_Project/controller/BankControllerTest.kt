642db33a63cde6e2e08218c6a4050f34efc9a0b7
maria mengozzi <mengozzi.maria@gmail.com>
2024-07-17 09:22:26 +0200

    feat: add patch method for updating a bank

--- src/test/kotlin/com/example/first_SpringBoot_kotlin_Project/controller/BankControllerTest.kt
+++ src/test/kotlin/com/example/first_SpringBoot_kotlin_Project/controller/BankControllerTest.kt
@@ -2,8 +2,6 @@ package com.example.first_SpringBoot_kotlin_Project.controller
 
 import com.example.first_SpringBoot_kotlin_Project.model.Bank
 import com.fasterxml.jackson.databind.ObjectMapper
-import jdk.jfr.ContentType
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -12,9 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
-import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.*
 
 @SpringBootTest //it create the whole spring project here we are testing the REST API
 @AutoConfigureMockMvc
@@ -63,6 +59,14 @@ internal class BankControllerTest @Autowired constructor(
                     content { contentType(MediaType.APPLICATION_JSON) }
                     jsonPath("$.trust"){ value("3.14") }
                     jsonPath("$.transactionFee"){ value("17") }
+                    /*alternatively we can create an object with these parameters and then compare the entire object
+                    with the body content:
+                    val bank = Bank("1234", 3.14, 17)
+                    .... content{
+                        { contentType(MediaType.APPLICATION_JSON) }
+                        json(objectMapper.writeValueAsString(bank))
+                    }
+                    */
                 }
         }
     }
@@ -85,7 +89,7 @@ internal class BankControllerTest @Autowired constructor(
     }
 
     @Nested
-    @DisplayName("POST /api/bank")
+    @DisplayName("POST /api/banks")
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class PostNewBank {
         @Test
@@ -101,16 +105,27 @@ internal class BankControllerTest @Autowired constructor(
             performPost.andDo { print() }
             .andExpect {
                 status { isCreated() }
-                content { contentType(MediaType.APPLICATION_JSON) }
-                jsonPath("$.accountNumber"){value("acc123")}
+                content {
+                    contentType(MediaType.APPLICATION_JSON)
+                    json(objectMapper.writeValueAsString(newBank))
+                }
+                /*jsonPath("$.accountNumber"){value("acc123")} //we check the entire body object with the json in the content
                 jsonPath("$.trust"){value("31.415")}
-                jsonPath("$.transactionFee"){value("2")}
+                jsonPath("$.transactionFee"){value("2")}*/
             }
+
+            //we verify that the bank is correctly updated using the route get > api/banks/{accountNum}
+            mockMvc.get("$baseUrl/${newBank.accountNumber}")
+                .andExpect {
+                    content {
+                        json(objectMapper.writeValueAsString(newBank))
+                    }
+                }
         }
     }
 
     @Nested
-    @DisplayName("POST /api/bank - BAD REQUEST")
+    @DisplayName("POST /api/banks - BAD REQUEST")
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     inner class PostNewBankBadRequest {
         @Test
@@ -130,5 +145,62 @@ internal class BankControllerTest @Autowired constructor(
         }
     }
 
+    @Nested
+    @DisplayName("PATCH /api/banks")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class PatchExistingBank {
+        @Test
+        fun `should update an existing bank`() {
+            //given
+            val updatedBank = Bank("1234", 1.0, 1)
+            //when
+            val performPatch = mockMvc.patch(baseUrl){
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(updatedBank)
+            }
+            //then
+            performPatch
+                .andDo { print() }
+                .andExpect {
+                    status {
+                        status { isOk() }
+                        content {
+                            contentType(MediaType.APPLICATION_JSON)
+                            json(objectMapper.writeValueAsString(updatedBank)) //we are checking the input that is in the body instead checking all single object property
+                        }
+
+                    }
+                }
+
+            //we verify that the bank is correctly updated using the route get > api/banks/{accountNum}
+            mockMvc.get("$baseUrl/${updatedBank.accountNumber}")
+                .andExpect {
+                    content {
+                        json(objectMapper.writeValueAsString(updatedBank))
+                    }
+                }
+        }
+    }
+
+    @Nested
+    @DisplayName("PATCH /api/banks - NOT FOUND")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class PatchNotExistingBank {
+        @Test
+        fun `should return NOT FOUND if no bank with the given number exist`() {
+            //given
+            val invalidBank = Bank("does_not_exist", 1.0, 1)
+            //when
+            val performPatch = mockMvc.patch(baseUrl){
+                contentType = MediaType.APPLICATION_JSON
+                content = objectMapper.writeValueAsString(invalidBank)
+            }
+            //then
+            performPatch.andDo { print() }
+                .andExpect {
+                    status { status { isNotFound() } }
+                }
+        }
+    }
 
 }
